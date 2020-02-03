834b3d163136e3b1eff8981d2f45afb1b6f2667e
Ali <alimahad.eng@gmail.com>
2020-02-03 14:30:07 +0000

    first commit

---
+++ src/main/java/com/qa/tutorial/Interaction.java
@@ -0,0 +1,25 @@
+package com.qa.tutorial;
+
+public class Interaction {
+
+
+
+			public String greeting() {
+				return "Hi friend";
+			}
+			
+			/**
+			 * This method is for people in your class
+			 * @return an insult
+			 */
+			public String insult() {
+				return "Is trash";
+			}
+			
+
+		
+
+
+	}
+
+
---
+++ src/main/java/com/qa/tutorial/Runner.java
@@ -0,0 +1,9 @@
+package com.qa.tutorial;
+
+class Runner {
+	public static void main(String[] args) {
+		Interaction potato = new Interaction();
+		System.out.println(potato.greeting() + " " + potato.insult());
+	}
+
+}
